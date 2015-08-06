331b50bdf2dae53d5ec439ff29e224fd1a9fc441
Brice Delvallee <bd@gmail.com>
2015-08-06 09:54:52 +0300

    Upgrade angular2 alpha 33

--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,4 @@
-import 'package:angular2/angular2.dart';
+import 'package:angular2/bootstrap.dart';
 import 'package:angular2/src/reflection/reflection.dart' show reflector;
 import 'package:angular2/src/reflection/reflection_capabilities.dart'
   show ReflectionCapabilities;
